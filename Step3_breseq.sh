2ed99d77b6348dd432b074c6efd15438cfa09274
Elizabeth Jerison <ejerison@stanford.edu>
2017-12-28 14:58:15 -0500

    typo fix

--- Step3_breseq.sh
+++ Step3_breseq.sh
@@ -3,10 +3,10 @@
 mkdir -p breseq_output
 
 #for i in $(python scripts/file_name_utility.py sample_list); do
-for i in ${"E1-1"}; do
-    export i
+#for i in ${"E1-1"}; do
+    export i=E1-1
     echo $i
     export FASTQ_FILES=$(python sequencing_analysis_12_28_2017/file_name_utility.py $i)
     export REF_SEQ=~/w303_reference_genome/w303_ref_with_seq.gff
     sbatch --job-name=breseq-${i} scripts/Step3_breseq.sbatch
-done
+#done
