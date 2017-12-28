7570f758a3151672449ad8c2a82f799b4f64ad51
Elizabeth Jerison <ejerison@stanford.edu>
2017-12-28 14:55:42 -0500

    path loc fix

--- Step3_breseq.sh
+++ Step3_breseq.sh
@@ -6,7 +6,7 @@ mkdir -p breseq_output
 for i in ${E1-1}; do
     export i
     echo $i
-    export FASTQ_FILES=$(python scripts/file_name_utility.py $i)
+    export FASTQ_FILES=$(python sequencing_analysis_12_28_2017/file_name_utility.py $i)
     export REF_SEQ=~/w303_reference_genome/w303_ref_with_seq.gff
     sbatch --job-name=breseq-${i} scripts/Step3_breseq.sbatch
 done
