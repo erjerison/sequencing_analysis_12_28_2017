c2bbcb0fb03fd1192c2c08e0f3997462be71aa84
Elizabeth Jerison <ejerison@stanford.edu>
2017-12-28 10:00:29 -0800

    updated script folder name

--- Step2_trim_adapters.sh
+++ Step2_trim_adapters.sh
@@ -4,5 +4,5 @@ mkdir trimmed_fastq_files
 for i in $(python sequencing_analysis_12_28_2017/file_name_utility.py trial); do
     export i
     echo $i
-    sbatch --job-name=trim-${i} scripts/Step2_trim_adapters.sbatch
+    sbatch --job-name=trim-${i} sequencing_analysis_12_28_2017/Step2_trim_adapters.sbatch
 done
