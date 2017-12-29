3ece90c4016cd62ec656f0eb7871d20f6829b850
Elizabeth Jerison <ejerison@stanford.edu>
2017-12-28 20:17:44 -0500

    renamed reference genome from .gff to .gff3

--- Step3_breseq.sh
+++ Step3_breseq.sh
@@ -7,6 +7,6 @@ mkdir -p breseq_output
     export i=E1-1
     echo $i
     export FASTQ_FILES=$(python sequencing_analysis_12_28_2017/file_name_utility.py $i)
-    export REF_SEQ=~/w303_reference_genome/w303_ref_with_seq_no_2micron.gff
+    export REF_SEQ=~/w303_reference_genome/w303_ref_with_seq_no_2micron.gff3
     sbatch --job-name=breseq-${i} sequencing_analysis_12_28_2017/Step3_breseq.sbatch
 #done
